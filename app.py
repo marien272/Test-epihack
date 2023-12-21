a53d008215392d9df4df0384ad7d7dc9dd80bcf9
marien olympio <marien.olympio@epitech.eu>
2023-12-21 13:09:36 +0100

    sfe

--- app.py
+++ app.py
@@ -2,25 +2,21 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# ... Autres configurations ...
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Vulnérabilité SQL Injection (à des fins éducatives uniquement)
 @app.route('/login', methods=['POST'])
 def login():
     username = request.form.get('username')
     password = request.form.get('password')
 
-    # Attention: Ne jamais utiliser cette approche en production
-    query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
-    # Exécutez la requête SQL et vérifiez si l'utilisateur est authentifié
-
-    # ... Logique d'authentification ...
-
-    return "Login successful"  # Cela doit être remplacé par une redirection ou une réponse appropriée
+    if username == 'D&dstr0ke77' and password == 'Epiteqtkek1':
+        # Utilisateur authentifié, affichez le message
+        return render_template('success.html', flag='ctf{sefb552}')
+    else:
+        # Mauvais identifiants, redirigez vers la page d'accueil
+        return render_template('index.html', error='Invalid username or password')
 
 if __name__ == '__main__':
     app.run(debug=True)
